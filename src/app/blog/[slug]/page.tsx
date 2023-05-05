d24b3d929f0142b114b042eae7820b5fbf68d5ce
Xeramiya <52908166+xeramiya@users.noreply.github.com>
2023-05-06 02:17:17 +0900

    blog bug fix

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,19 +3,44 @@ import DateFormatter from "components/DateFormatter";
 
 const pieceType = "blog";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const pieceData = getPieceData(`piece/${pieceType}`, params.slug);
-
-  return {
-    title: pieceData.frontMatter.title,
-    description: pieceData.frontMatter.synopsis,
-  };
-}
+const ModifiedDate = ({ date }: { date: string | undefined }) => {
+  if (date) {
+    return (
+      <span className="text-sm">
+        &nbsp;/ 最終更新
+        <DateFormatter date={date} />
+      </span>
+    );
+  } else {
+    return null;
+  }
+};
 
 export default async function Piece({ params }: { params: { slug: string } }) {
-  return <div className="stand">よう！</div>;
+  const pieceData = getPieceData(`piece/${pieceType}`, params.slug, true);
+  const pieceContent = await mdAdapter(pieceData.content);
+
+  return (
+    <div className="stand">
+      <article className="mx-6 mt-12">
+        <section>
+          <div className="mt-3 text-char-note">
+            <span className="text-base">
+              <DateFormatter date={pieceData.frontMatter.date.created} />
+            </span>
+            <ModifiedDate date={pieceData.frontMatter.date.modified} />
+          </div>
+          <h1 className="mt-2">{pieceData.frontMatter.title}</h1>
+          <div className="mt-9">{pieceData.frontMatter.synopsis}</div>
+        </section>
+        <hr className="mt-4 border-char-note mb-11" />
+        <section className="piece max-w-[712px] ml-0">
+          <div
+            className=""
+            dangerouslySetInnerHTML={{ __html: pieceContent }}
+          />
+        </section>
+      </article>
+    </div>
+  );
 }
