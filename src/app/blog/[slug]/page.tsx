f4786783142c1dcf11c7404f5b79a74872983905
Xeramiya <52908166+xeramiya@users.noreply.github.com>
2023-05-06 02:15:06 +0900

    blog bug fix

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,6 +3,19 @@ import DateFormatter from "components/DateFormatter";
 
 const pieceType = "blog";
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const pieceData = getPieceData(`piece/${pieceType}`, params.slug);
+
+  return {
+    title: pieceData.frontMatter.title,
+    description: pieceData.frontMatter.synopsis,
+  };
+}
+
 export default async function Piece({ params }: { params: { slug: string } }) {
   return <div className="stand">よう！</div>;
 }
