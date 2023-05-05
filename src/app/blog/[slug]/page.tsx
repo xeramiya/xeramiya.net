f12f7ec51d20e98170e771b9e22fcb3f9e0fcbe5
Xeramiya <52908166+xeramiya@users.noreply.github.com>
2023-05-06 03:48:50 +0900

    blog bug fix

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -43,12 +43,12 @@ export default async function Piece({ params }: { params: { slug: string } }) {
         <section>
           <div className="mt-3 text-char-note">
             <span className="text-base">
-              <DateFormatter date={pieceData.frontMatter.date.created} />
+
             </span>
             
           </div>
-          <h1 className="mt-2">{pieceData.frontMatter.title}</h1>
-          <div className="mt-9">{pieceData.frontMatter.synopsis}</div>
+
+
         </section>
         <hr className="mt-4 border-char-note mb-11" />
         <section className="piece max-w-[712px] ml-0">
