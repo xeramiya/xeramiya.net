440d4d6f998f7bcb8bca0ce26f580a22077e65b5
Xeramiya <52908166+xeramiya@users.noreply.github.com>
2023-07-28 22:22:07 +0900

    rainbow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
           <section>
             <div className="ultra-wide flex justify-center p-5 pt-16">
               <div className="slate:w-screen-slate container relative h-[80vh] min-h-[20rem] w-full overflow-hidden rounded-xl fill-char-main text-char-main slate:min-w-[61.8%]">
-                <div className="absolute h-full w-full rounded-xl bg-media-invert opacity-5"></div>
+                <div className="absolute h-full w-full rounded-xl bg-media-invert opacity-10"></div>
                 <div className="hyper-stitch-1 absolute h-full w-full rounded-xl border-media-invert"></div>
                 <div className="hyper-stitch-2 absolute h-full w-full rounded-xl border-media-invert"></div>
                 <div className="hyper-stitch-3 absolute h-full w-full rounded-xl border-media-invert"></div>
-                <div className="absolute h-full w-full rounded-xl backdrop-blur-lg"></div>
+                <div className="absolute h-full w-full rounded-xl backdrop-blur-xl"></div>
                 <XeramiyaNetIcon className="container absolute left-1/2 top-1/2 h-40 -translate-x-1/2 -translate-y-1/2" />
                 <div className="absolute bottom-0 right-0 px-4 py-6 font-serif text-3xl">
                   <span className="inline-block">ようこそ、</span>
